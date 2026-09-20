943c904cdf6071abf8ff09b608084a917bc43d72
M. A. Sanchez <masanche@nrao.edu>
2026-09-20 05:54:52 -0400

    Add files via upload

---
+++ ir_model_ms/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+from ir_model_ms.intensity import Intensity
+from ir_model_ms.moldata import MolData
+from ir_model_ms.spectrum import Spectrum
+from ir_model_ms.chi2spectrum import Chi2Spectrum, FluxMeasurement
